9e738dc252ba99f377347f15f938299e01c4900c
jesse skralskis <jesseskralskis@gmail.com>
2020-06-11 16:48:15 -0400

    production build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
-let activeEnv =
-  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+// let activeEnv =
+//   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 
-console.log("Using environment config: ${activeEnv}")
+// console.log("Using environment config: ${activeEnv}")
 
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
